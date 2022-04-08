7628315c96d30e81e0e5a6a833baee51e0af7406
Zom-23 <90399747+Zom-23@users.noreply.github.com>
2022-04-08 08:33:57 -0500

    grenade launcher

--- CommCards/Cards/Pudassassin.cs
+++ CommCards/Cards/Pudassassin.cs
@@ -1,13 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using UnboundLib;
+﻿using System.Collections.Generic;
 using UnboundLib.Cards;
 using UnityEngine;
-using UnboundLib.GameModes;
+using System.Linq;
+using CommCards.MonoBehaviours;
+using System.Reflection;
+using Photon.Pun;
+using System.Collections.ObjectModel;
+using UnboundLib.Utils;
 using CommCards.Extensions;
+using HarmonyLib;
+using System.Runtime.CompilerServices;
+using System;
+using InControl;
+using ModdingUtils.MonoBehaviours;
+using UnboundLib;
+using System.Collections;
 
 namespace CommCards.Cards
 {
@@ -16,13 +23,8 @@ namespace CommCards.Cards
     {
         public override void OnAddCard(Player player, Gun gun, GunAmmo gunAmmo, CharacterData data, HealthHandler health, Gravity gravity, Block block, CharacterStatModifiers characterStats)
         {
-            player.gameObject.GetOrAddComponent<Explosion>();
-            Explosion explosion = player.gameObject.GetComponent<Explosion>();
-            gun.AddAttackAction(DoExplosion);
-            void DoExplosion()
-            {
-                explosion.Explode();
-            }
+            player.gameObject.GetOrAddComponent<GrenadeLaunch>();
+            
         }
 
         public override void SetupCard(CardInfo cardInfo, Gun gun, ApplyCardStats cardStats, CharacterStatModifiers statModifiers, Block block)
@@ -86,29 +88,78 @@ namespace CommCards.Cards
     {
         Player player;
         Gun gun;
+        ReversibleEffect grenade;
         void Start()
         {
             player = gameObject.GetComponent<Player>();
             gun = player.data.weaponHandler.gun;
+            
         }
 
         void Go()
         {
             if (!PlayerStatus.PlayerAliveAndSimulated(player))
                 return;
-            
+            buildGrenade();
+            this.player.data.weaponHandler.gun.Attack(0f, true, gun.damage, 1f, false);
         }
 
-        public HasToReturn DoHitEffect(HitInfo hit)
+        void buildGrenade()
         {
-            if (!hit.transform)
+            grenade = new ReversibleEffect();
+            grenade.gun.damage *= 2f;
+
+
+            //Code taken from Boss Sloth
+            var explosiveBullet = (GameObject)Resources.Load("0 cards/Explosive bullet");
+            var a_Explosion = explosiveBullet.GetComponent<Gun>().objectsToSpawn[0].effect;
+            var explo = Instantiate(a_Explosion);
+            explo.transform.position = new Vector3(1000, 0, 0);
+            explo.hideFlags = HideFlags.HideAndDontSave;
+            explo.name = "customExplo";
+            DestroyImmediate(explo.GetComponent<RemoveAfterSeconds>());
+            var explodsion = explo.GetComponent<Explosion>();
+            explodsion.force = 100000;
+            gun.damage = 2f;
+
+            grenade.gun.objectsToSpawn = new[]
             {
-                return HasToReturn.canContinue;
-            }
+                new ObjectsToSpawn
+                {
+                    effect = explo,
+                    normalOffset = 0.1f,
+                    numberOfSpawns = 1,
+                    scaleFromDamage = .5f,
+                    scaleStackM = 0.7f,
+                    scaleStacks = true,
+                },
+                new ObjectsToSpawn
+                {
+                    scaleFromDamage = 0f
+                }
+            };
+        }
+    }
 
-            
+    [HarmonyPatch(typeof(PlayerActions))]
+    [HarmonyPatch(MethodType.Constructor)]
+    [HarmonyPatch(new Type[] { })]
+    class PlayerActionsPatchPlayerActions
+    {
+        private static void Postfix(PlayerActions __instance)
+        {
+            __instance.GetAdditionalData().switchWeapon = (PlayerAction)typeof(PlayerActions).InvokeMember("CreatePlayerAction",
+                                    BindingFlags.Instance | BindingFlags.InvokeMethod |
+                                    BindingFlags.NonPublic, null, __instance, new object[] { "Switch Weapon" });
 
-            return HasToReturn.canContinue;
+        }
+    }
+    [HarmonyPatch(typeof(PlayerActions), "CreateWithKeyboardBindings")]
+    class PlayerActionsPatchCreateWithKeyboardBindings
+    {
+        private static void Postfix(ref PlayerActions __result)
+        {
+            __result.GetAdditionalData().switchWeapon.AddDefaultBinding(Key.G);
         }
     }
 }
